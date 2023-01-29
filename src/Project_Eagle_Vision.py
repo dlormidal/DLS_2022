5e43b643e6a6e354640aee2d029390800dd89be5
dlormidal <108503395+dlormidal@users.noreply.github.com>
2023-01-30 00:42:21 +0900

    Update Project_Eagle_Vision.py

--- src/Project_Eagle_Vision.py
+++ src/Project_Eagle_Vision.py
@@ -19,6 +19,8 @@ import torch, torchvision
 import mmdet
 import mmcv
 #from mmcv.ops import get_compiling_cuda_version, get_compiler_version
+print(torch.__version__)
+print(torch.version.cuda)
 from mmdet.apis import inference_detector, init_detector, show_result_pyplot
 #from mmdet.apis.inference import inference_detector
 #print(dir(mmdet))
