c13ddbd84e4b46e2efa94d645038a5c7723e2e0c
dlormidal <108503395+dlormidal@users.noreply.github.com>
2023-01-30 12:54:41 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+pip install streamlit==0.77.0
+pip install boto3==1.17.3
+
+pip install Pillow
+pip install pandas
+pip install torch
+pip install torchvision
+pip install opencv_python
+
+pip install -U openmim
+mim install mmcv-full
+
+git clone https://github.com/open-mmlab/mmdetection.git
+cd mmdetection
+pip install -v -e .
+
+pip install mmdet
